610366804bd98c0fa480aff4e11020dd364942f5
Tinh36 <tinh3244@gmail.com>
2023-12-29 16:54:03 +0700

    do an tich hop

---
+++ data/products.js
@@ -0,0 +1,382 @@
+var list_products = [{
+    "name": "Thức ăn cho chó",
+    "company": "Fet Shop",
+    "img": "img/products/h1.jpg",
+    "price": "100.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "freeship",
+        "value": "miễn phí vận chuyển"
+    },
+    "detail": {
+        "screen": "Thức ăn khô",
+        "os": "dành cho chó dưới 1 tuổi",
+        "camara": "aaa",
+       
+    },
+    "masp": "DF"
+}, {
+    "name": "Thức ăn cho mèo",
+    "company": "Fet Shop",
+    "img": "img/products/h7.jpg",
+    "price": "100.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "freeship",
+        "value": "miễn phí vận chuyển"
+    },
+    "detail": {
+        "screen": "Thức ăn khô",
+        "os": "Dành cho mèo",
+        "camara": "aaaa",
+       
+    },
+    "masp": "CF1"
+}, {
+    "name": "Thức ăn chó mèo",
+    "company": "Hạt Food",
+    "img": "img/products/h8.jpg",
+    "price": "100.000",
+    "star": 5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "500.000"
+    },
+    "detail": {
+        "screen": "Hạt",
+        "os": " Dành cho chó mèo",
+        "camara": "asd",
+        
+    },
+    "masp": "Hat0"
+}, {
+    "name": "Hạt vị truyền thống",
+    "company": "Hạt Food",
+    "img": "img/products/tinh.jpg",
+    "price": "100.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "giamgia",
+        "value": "250.000"
+    },
+    "detail": {
+        "screen": "Hạt",
+        "os": "Dành cho chó mèo",
+        "camara": "ádsads",
+        
+    },
+    "masp": "Hat"
+}, {
+    "name": "Hạt vị gà",
+    "company": "Hạt Food",
+    "img": "img/products/tinh2.jpg",
+    "price": "11.990.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.500.000"
+    },
+    "detail": {
+        "screen": "Hạt",
+        "os": "Dành cho chó mèo",
+        "camara": "ewq",
+        
+    },
+    "masp": "hat1"
+}, {
+    "name": "Hạt vị bò",
+    "company": "Hạt Food",
+    "img": "img/products/tinh2.jpg",
+    "price": "100.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "giareonline",
+        "value": "50.000"
+    },
+    "detail": {
+        "screen": "Hạt",
+        "os": "Dành cho chó mèo",
+        "camara": "ádsad",
+       
+    },
+    "masp": "Hat3"
+}, {
+    "name": "Hạt vị cá",
+    "company": "Hạt Food",
+    "img": "img/products/tinh.jpg",
+    "price": "100.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Hạt",
+        "os": "Dành cho chó mèo",
+        "camara": "aaaaa",
+        
+    },
+    "masp": "Hat4"
+}, {
+    "name": "Thức ăn cho chó và mèo",
+    "company": "Fet Food",
+    "img": "img/products/h1.jpg",
+    "price": "100.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "50.000"
+    },
+    "detail": {
+        "screen": "THức ăn khô",
+        "os": "Dành cho chó và mèo",
+        "camara": "ssssss",
+       
+    },
+    "masp": "CD2"
+}, {
+    "name": "Thức ăn cho mèo và chó",
+    "company": "Fet Food",
+    "img": "img/products/h7.jpg",
+    "price": "100.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Thức awnh ướt'",
+        "os": "Dành cho chó mèo",
+        "camara": "ssssss",
+      
+    },
+    "masp": "CD3"
+}, {
+    "name": "THức ăn nhanh",
+    "company": "Fet Food",
+    "img": "img/products/h8.jpg",
+    "price": "100.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "50.000"
+    },
+    "detail": {
+        "screen": "Thức ăn khô",
+        "os": "Dành cho chó mèo",
+        "camara": "aaaaa",
+       
+    },
+    "masp": "CD4"
+}, {
+    "name": "Chó Và Mèo",
+    "company": "Fet Food",
+    "img": "img/products/h1.jpg",
+    "price": "100.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Thức ăn Ướt",
+        "os": "Dành cho chó và mèo",
+        "camara": "sssssss",
+       
+    },
+    "masp": "CD5"
+}, {
+    "name": "Mèo Và chó",
+    "company": "Fet Food",
+    "img": "img/products/h8.jpg",
+    "price": "100.000",
+    "star": 4,
+    "rateCount": 16,
+    "promo": {
+        "name": " ",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "Thức ăn khô",
+        "os": "dành cho chó mèo",
+        "camara": "aaaaaa",
+        
+    },
+    "masp": "CD6"
+}, {
+    "name": "Thức ăn ",
+    "company": "Fet Food",
+    "img": "img/products/tinh.jpg",
+    "price": "100.000",
+    "star": 4,
+    "rateCount": 104,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "Thức ăn khô",
+        "os": "dành cho chó mèo",
+        "camara": "aaaaaa",
+        
+    },
+    "masp": "CD7"
+}, {
+    "name": "đồ ăn thú cưng",
+    "company": "Fet Food",
+    "img": "img/products/tinh2.jpg",
+    "price": "100.00",
+    "star": 4,
+    "rateCount": 80,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "Thứ ăn ướt",
+        "os": "Dành cho chó mèo",
+        "camara": "qưe",
+       
+    },
+    "masp": "CD8"
+}, {
+    "name": "Thức ăn thú cưng",
+    "company": "Fet Food",
+    "img": "img/products/h1.jpg",
+    "price": "260.000",
+    "star": 5,
+    "rateCount": 87,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "Thức ăn ướt",
+        "os": "Dành cho chó mèo",
+        "camara": "ttttt",
+       
+    },
+    "masp": "CD9"
+}, {
+    "name": "Thức ăn mèo sơ sinh ",
+    "company": "Fet Food",
+    "img": "img/products/h7.jpg",
+    "price": "100.000",
+    "star": 4,
+    "rateCount": 372,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "screen": "Thức ăn ướt",
+        "os": "Dành cho mèo sơ sinh",
+        "camara": "hghghghh",
+      
+    },
+    "masp": "CF7"
+}, {
+    "name": "Thức ăn cho mèo trưởng thành",
+    "company": "Fet Food",
+    "img": "img/products/h8.jpg",
+    "price": "100.000",
+    "star": 4,
+    "rateCount": 347,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "Thức ăn khô",
+        "os": "Dành cho mèo trưởng thành",
+        "camara": "aaa",
+       
+    },
+    "masp": "CF8"
+}, {
+    "name": "Thức ăn cho mèo kén ăn",
+    "company": "Fet Food",
+    "img": "img/products/h1.jpg",
+    "price": "100.000",
+    "star": 5,
+    "rateCount": 4,
+    "promo": {
+        "name": "",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "THức ăn ướt",
+        "os": "Dành cho mèo lười ăn",
+        "camara": "gggg",
+       
+    },
+    "masp": "CF9"
+}, {
+    "name": "Thức ăn cho chó nhỏ",
+    "company": "Fet Food",
+    "img": "img/products/tinh.jpg",
+    "price": "100.00",
+    "star": 4,
+    "rateCount": 22,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "screen": "Thức ăn Ướt",
+        "os": "Dành cho chó còn nhỏ",
+        "camara": "ttttt",
+      
+    },
+    "masp": "DF7"
+},{
+	"name": "hat",
+	"company": "Hạt",
+	"img": "img/products/h1.jpg",
+	"price": "100.000",
+	"star": "5",
+	"rateCount": "20",
+	"promo": {
+		"name": "moiramat",
+		"value": ""
+	},
+    "detail": {
+        "screen": "Hạt",
+        "os": "Dành cho chó còn nhỏ",
+        "camara": "ttttt",
+      
+    },
+    "masp": "PF7"
+
+},
+{
+	"name": "Hạt chó và mèo",
+	"company": "Hạt",
+	"img": "img/products/h1.jpg",
+	"price": "100.000",
+	"star": "5",
+	"rateCount": "20",
+	"promo": {
+		"name": "giamgia",
+		"value": "12.000"
+	}, 
+    "detail": {
+        "screen": "Hạt",
+        "os": "Dành cho chó còn nhỏ",
+        "camara": "ttttt",
+      
+    },
+    "masp": "FF1"
+}
+]
